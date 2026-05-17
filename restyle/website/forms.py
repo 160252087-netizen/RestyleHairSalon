a8aedcef2c00318f7aa12d6b4c0ab150cf284719
Nezarion <160252087@student.rnngroup.ac.uk>
2026-05-17 15:50:42 +0100

    new updates

---
+++ restyle/website/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Product, BlogPost, BandA
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price', 'image']
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ['name', 'blog', 'image']
+
+class BandAForm(forms.ModelForm):
+    class Meta:
+        model = BandA
+        fields = ['name', 'before', 'after'] 
+
+widgets = {
+    'title': forms.TextInput(attrs={
+        'class': 'form-control'
+    }),
+    'description': forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 5
+    }),
+}
